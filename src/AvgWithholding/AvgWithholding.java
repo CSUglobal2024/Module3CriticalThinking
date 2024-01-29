08cfc774ca36b2cb891615fc8d2332cee5adffb2
Debbie King <debbieking1979@gmail.com>
2024-01-29 12:42:39 -0500

    added varaibles income and tax and added if else for tax brackets

--- src/AvgWithholding/AvgWithholding.java
+++ src/AvgWithholding/AvgWithholding.java
@@ -1,4 +1,5 @@
 package AvgWithholding;
+import java.util.Scanner;
 
 public class AvgWithholding {
 
@@ -15,18 +16,46 @@ public class AvgWithholding {
 //			application executing the application, the results and GIT repository in a single document.
 //			
 		/*
-		 * Plan booleans and comparison operators. Going to need scanner. Should I use switch statement or
-		 * if else for the logic to compare input to tax brackets?
-		 * Should I use fixed variables for the tax rates?
+		 * Plan booleans and comparison operators. Going to need scanner. 
+		 * Should I use fixed variables for the tax rates, or just multiply by the respective decimals?
+		 * 
+		 * Before coding - use git init and make first add and commit, connect to a new repository on my github
+		 * Then push. Continue to commit changes regularly with informative commit notes.
 		 * 
 		 * Step 1. import scanner
 		 * Step 2. initialize variable for income input
-		 * Step 3. create switch statement(s) for each income bracket, starting with the smallest
+		 * Step 3. create if/else statement(s) for each income bracket, starting with the smallest
 		 * 			and only saying less than the max for each successive bracket, because the branches
-		 * 			 will not continue to be read with switch statements that have breaks. Might be better 
-		 * 			than if/else.
+		 * 			 will not continue to be read.
 		 * Step 3. close scanner to prevent leaks.
+		 * 
+		 *  - variable for income and tax withholding will be a double as Float runs out of precision to reliably store 
+		 *  	values to the cent somewhere in the mid $80k range. Most people do not make 80k a week, but
+		 *  	some do.
+		 * 
+		 * - 
 		 */
+		Scanner scnr = new Scanner(System.in);
+		double income;
+		double taxWithholding;
+		System.out.println("Enter customer's gross weekly income in dollars: $");
+		
+		income = scnr.nextInt();
+		
+		if (income < 500) {
+			taxWithholding = income * 0.10;
+		} 
+		else if (income < 1500) {
+			taxWithholding = income * 0.15;
+		}
+		else if (income < 2500) {
+			taxWithholding = income * 0.20;
+		}
+		else {
+			taxWithholding = income * 0.30;
+		}
+		
+		System.out.println("Tax witholding per week is " + taxWithholding + ". ");
 	}
 
 }
